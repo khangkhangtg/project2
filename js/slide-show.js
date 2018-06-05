b8b2d4a5c08cb376b7d9fe09dd0e0a6a97ea98f6
khangkhangtg <khangkhangtg@gmail.com>
2018-06-05 23:33:50 +0700

    Add files via upload

--- js/slide-show.js
+++ js/slide-show.js
@@ -15,3 +15,15 @@ function changeSlide(n){
     slides[curSlide].style.display = 'block';
   }
 }
+
+$(function() {
+    $(".slideshow2").carouFredSel({
+        auto: {
+            pauseOnHover: 'resume'
+        },
+        // auto: false,
+        pagination: '.pagination',
+        prev: '.btn-prev-ss2',
+        next: '.btn-next-ss2'
+    }); 
+},{transition: true});
